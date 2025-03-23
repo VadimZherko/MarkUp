20c60cc4aa79f1a437f4eeffd934a33de9d07c00
Franchesko <pechenakzv@gmail.com>
2025-03-24 01:52:09 +0300

    Del build

--- build/CMakeFiles/qrhead.dir/qrhead_autogen/T24IUX3VL4/qrc_img.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/qrhead.dir/qrhead_autogen/T24IUX3VL4/qrc_img.cpp.o: \
- /home/vadim/repository/tesk_cmake/qrhead/build/qrhead_autogen/T24IUX3VL4/qrc_img.cpp \
- /usr/include/stdc-predef.h
